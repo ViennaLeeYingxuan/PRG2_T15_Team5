b02a590ea4a4a48b59c154a1b9cfffaabe6b2f36
Jue Ying Goh <s10189502@connect.np.edu.sg>
2019-01-17 12:50:02 +0800

    Complete Stay except calculateTotal

--- PRG2_T15_Team5/Stay.cs
+++ PRG2_T15_Team5/Stay.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace PRG2_T15_Team5
 {
-    class Stay
+    class Stay : HotelRoom
     {
-        private List<string> roomList;
+        private List<HotelRoom> roomList = new List<HotelRoom>();
         private DateTime checkInDate;
         private DateTime checkOutDate;
 
+        public List<HotelRoom> RoomList
+        {
+            get { return roomList; }
+            set { roomList = value; }
+        }
+
+        public DateTime CheckInDate
+        {
+            get { return checkInDate; }
+            set { checkInDate = value; }
+        }
+
+        public DateTime CheckOutnDate
+        {
+            get { return checkOutDate; }
+            set { checkOutDate = value; }
+        }
+
+        public Stay() { }
+        public Stay(DateTime In, DateTime Out)
+        {
+            checkInDate = In;
+            checkOutDate = Out;
+        }
+
+        public void AddRoom(HotelRoom r)
+        {
+            roomList.Add(r);
+        }
+
+        //public double CalculateTotal()
 
     }
 }
