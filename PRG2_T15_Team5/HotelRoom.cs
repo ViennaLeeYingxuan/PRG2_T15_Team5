4123a80bb28974ab043c405b0797b53ad5b1f87b
Yingxuan Vienna Lee <s10177256@connect.np.edu.sg>
2019-01-17 12:47:58 +0800

    properties of hotel room

--- PRG2_T15_Team5/HotelRoom.cs
+++ PRG2_T15_Team5/HotelRoom.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace PRG2_T15_Team5
 {
-    class HotelRoom
+    abstract class HotelRoom
     {
+        private string roomType;
+        private string roomNumber;
+        private string bedConfiguration;
+        private double dailyRate;
+        private bool isAvail;
+        private int noOfOccupants;
+
+        public string RoomType
+        {
+            get { return roomType; }
+            set { roomType = value; }
+        }
+
+        public string RoomNumber
+        {
+            get { return roomNumber; }
+            set { roomNumber = value; }
+        }
+
+        public string BedConfiguration
+        {
+            get { return bedConfiguration; }
+            set { bedConfiguration = value; }
+        }
+
+        public double DailyRate
+        {
+            get { return dailyRate; }
+            set { dailyRate = value; }
+        }
+
+        public bool IsAvail
+        {
+            get { return isAvail; }
+            set { isAvail = value; }
+        }
+
+        public int NoOfOccupants
+        {
+            get { return noOfOccupants; }
+            set { noOfOccupants = value; }
+        }
     }
 }
