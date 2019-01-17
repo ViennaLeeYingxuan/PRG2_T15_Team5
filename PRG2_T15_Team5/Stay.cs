2dd935993beb549101310df52cf0754c6f4805a3
Yingxuan Vienna Lee <s10177256@connect.np.edu.sg>
2019-01-17 12:04:05 +0800

    trial 1

---
+++ PRG2_T15_Team5/Stay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T15_Team5
+{
+    class Stay
+    {
+        //hiiiiiii
+    }
+}
